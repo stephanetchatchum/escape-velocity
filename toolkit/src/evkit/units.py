39668c3b4c20074f73f7b069f57969fbad0e0598
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-08-21 07:29:41 +0100

    Coumpound Units and reverse conversion dictionaries

--- toolkit/src/evkit/units.py
+++ toolkit/src/evkit/units.py
@@ -1,6 +1,33 @@
 class DimensionError(Exception):
     pass
 
+prefixes = {
+    'Q': 10**30, # Quetta (1 nonillion)
+    'R': 10**27, # Ronna (1 octillion)
+    'Y': 10**24, # Yotta (1 septillion)
+    'Z': 10**21, # Zetta (1 sextillion)
+    'E': 10**18, # Exa (1 quintillion)
+    'P': 10**15, # Peta (1 quadrillion)
+    'T': 10**12, # Tera (1 trillion)
+    'G': 10**9, # Giga (1 billion)
+    'M': 10**6, # Mega (1 million)
+    'k': 10**3, # Kilo (1 thousand)
+    'h': 10**2, # Hecta (1 hundred)
+    'da': 10**1, # Deca (10)
+    'd': 10**-1, # Deci (0.1)
+    'c': 10**-2, # Centi (0.01)
+    'm': 10**-3, # Milli (0.001)
+    'μ': 10**-6, # Micro (1 millionth)
+    'n': 10**-9, # Nano (1 billionth)
+    'p': 10**-12, # Pico (1 trillionth)
+    'f': 10**-15, # Femto (1 quadrillionth)
+    'a': 10**-18, # Atto (1 quintillionth)
+    'z': 10**-21, # Zepto (1 sextillionth)
+    'y': 10**-24, # Yocto (1 septillionth)
+    'r': 10**-27, # Ronto (1 octillionth)
+    'q': 10**-30, # Quecto (1 nonillionth)
+}
+
 class Quantity:
     def __init__(self, value, base_unit = (0,0,0,0,0,0,0)):
         self.value = value
@@ -68,6 +95,7 @@ class Quantity:
     def __neg__(self):
         return Quantity(-self.value, self.base_unit)
 
+
 BASE_UNITS = {
     'm'  : Quantity(1, (1,0,0,0,0,0,0)), # length (Metres)
     'kg' : Quantity(1, (0,1,0,0,0,0,0)), # mass (Kilograms)
@@ -75,5 +103,53 @@ BASE_UNITS = {
     'I'  : Quantity(1, (0,0,0,1,0,0,0)), # Electric current (Amperes)
     'K'  : Quantity(1, (0,0,0,0,1,0,0)), # Thermodynamic Temperature (Kelvin)
     'mol': Quantity(1, (0,0,0,0,0,1,0)), # Amount of substance (Mole)
-    'cd' : Quantity(1, (0,0,0,0,0,0,1))  # Luminous intensity (Candela)
+    'cd' : Quantity(1, (0,0,0,0,0,0,1)),  # Luminous intensity (Candela)
+
+    'g'  : Quantity(0.001, (0,1,0,0,0,0,0)) # mass (Grams)
 }
+
+COMPOUND_UNITS = {
+    'N' : Quantity(1, (1,1,-2,0,0,0,0)),  # Force (Newton)
+    'J' : Quantity(1, (2,1,-2,0,0,0,0)),  # Energy (Joule)
+    'W' : Quantity(1, (2,1,-3,0,0,0,0)),  # Power (Watt)
+    'Pa' : Quantity(1, (-1,1,-2,0,0,0,0)),# Pressure (Pascal)
+    'C' : Quantity(1, (0,0,1,1,0,0,0)),   # Electric Charge (Coulomb)
+    'V' : Quantity(1, (2,1,-3,-1,0,0,0)), # Electric Potential (Volt)
+    'Ω' : Quantity(1, (1,2,-3,-2,0,0,0)), # Electric Resistance (Ohm)
+    'T' : Quantity(1, (0,1,-2,-1,0,0,0)), # Magnetic Field (Tesla)
+    'Hz' : Quantity(1, (0,0,-1,0,0,0,0)), # Frequency (Hertz)
+}
+
+KNOWN_UNITS = {**BASE_UNITS, **COMPOUND_UNITS}
+
+REVERSE_LOOKUP = {
+    (1,0,0,0,0,0,0): 'm',
+    (0,1,0,0,0,0,0): 'kg',
+    (0,0,1,0,0,0,0): 's',
+    (0,0,0,1,0,0,0): 'I',
+    (0,0,0,0,1,0,0): 'K',
+    (0,0,0,0,0,1,0): 'mol',
+    (0,0,0,0,0,0,1): 'cd',
+    (1,1,-2,0,0,0,0): 'N',
+    (2,1,-2,0,0,0,0): 'J',
+    (2,1,-3,0,0,0,0): 'W',
+    (-1,1,-2,0,0,0,0): 'Pa',
+    (0,0,1,1,0,0,0): 'C',
+    (2,1,-3,-1,0,0,0): 'V',
+    (2,1,-3,-2,0,0,0): 'Ω',
+    (0,1,-2,-1,0,0,0): 'T',
+    (0,0,-1,0,0,0,0): 'Hz',
+}
+
+def parse_unit(unit_str):
+    if unit_str in KNOWN_UNITS:
+        return KNOWN_UNITS[unit_str]
+
+    for prefix, scale in prefixes.items():
+        if unit_str.startswith(prefix):
+            u = unit_str[len(prefix):]
+            if u in KNOWN_UNITS:
+                return scale * KNOWN_UNITS[u]
+
+    raise ValueError(f"Unknown unit: {unit_str}")
+    
